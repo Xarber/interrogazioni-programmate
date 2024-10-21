8433caed9cae3b62911cc5d62ceedb105dea0334
Xarber <62101357+Xarber@users.noreply.github.com>
2024-10-21 10:49:19 +0200

    TEST2

--- dash.js
+++ dash.js
@@ -764,9 +764,10 @@ class AdminDashboard {
                 alert(`Questa data è già esistente!`);
                 return this.addDay();
             }
-            let dayName = new Date(`${date.split("-")[1]}-${date.split("-")[0]}-${date.split("-")[2]}`).toLocaleString("it-IT", {weekday: "long"});
+            const formattedDate = `${date.split("-")[1]}-${date.split("-")[0]}-${date.split("-")[2]}`;
+            let dayName = (new Date(formattedDate)).toLocaleString("it-IT", {weekday: "long"});
             dayName = dayName.substring(0, 1).toUpperCase() + dayName.substring(1, dayName.length);
-            alert("Data: " + dayName + " " + date);
+            alert("Data: " + formattedDate + " " + dayName + " " + date);
             let availability = prompt('Quanti posti dovrebbero essere disponibili? (Ex. 3):');
             if (dayName && availability) {
                 if (availability.length < 1) availability = "3";
