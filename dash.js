273170133d99ee5fd7cee68472ef8a49c68180c5
Xarber <nintendoswitch01234@gmail.com>
2024-10-22 14:19:34 +0200

    Fix again

--- dash.js
+++ dash.js
@@ -505,7 +505,7 @@ class AdminDashboard {
             const profileElement = document.createElement('div');
             profileElement.className = 'admin-day-item';
             profileElement.innerHTML = `
-                <span onclick="if (confirm('Sei sicuro di voler cambiare il profilo su ${e}?')) location.href = location.href.split('?')[0]+'?profile=${e}&UID'+(new URLSearchParams(location.search).get('UID'));">${e}</span>
+                <span onclick="if (confirm('Sei sicuro di voler cambiare il profilo su ${e}?')) location.href = location.href.split('?')[0]+'?profile=${e}&UID='+(new URLSearchParams(location.search).get('UID'));">${e}</span>
                 <div class="admin-inline admin-user-actions">
                     <button class="admin-edit-day-btn" data-profile="${e}">
                         <svg xmlns="http://www.w3.org/2000/svg" height="20px" viewBox="0 -960 960 960" width="20px" fill="#e8eaed"><path d="M216-216h51l375-375-51-51-375 375v51Zm-72 72v-153l498-498q11-11 23.84-16 12.83-5 27-5 14.16 0 27.16 5t24 16l51 51q11 11 16 24t5 26.54q0 14.45-5.02 27.54T795-642L297-144H144Zm600-549-51-51 51 51Zm-127.95 76.95L591-642l51 51-25.95-25.05Z"/></svg>
