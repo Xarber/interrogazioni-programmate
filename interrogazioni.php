9d8c60f416587db0ae5f33c627af5e047039703d
Xarber <nintendoswitch01234@gmail.com>
2024-10-28 15:37:58 +0100

    Add manifest for PWA test cause apple is dumb

--- interrogazioni.php
+++ interrogazioni.php
@@ -344,6 +344,7 @@ foreach ($subjectJSONs as $subjectNameTMP) {
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="manifest" href="manifest.json">
     <title>Prenota Interrogazioni</title>
     <style>
         <?php echo file_get_contents("app.css") ?>
