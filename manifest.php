45320a0c58e5c29a744d1a77d22c71ef9e22acd8
Xarber <nintendoswitch01234@gmail.com>
2024-10-29 14:16:41 +0100

    Another manifest update

--- manifest.php
+++ manifest.php
@@ -8,46 +8,12 @@ header("Content-Type: application/json");
 session_start();
 $startUrl = ($_SESSION["lastAccessID"] ?? "/interrogazioni.php");
 if (!str_contains($startUrl, "UID=")) $startUrl = $startUrl."?".$_SERVER["QUERY_STRING"];
-/*
-{
-  "name": "Interrogazioni Programmate",
-  "short_name": "Interrogazioni",
-  "start_url": "/interrogazioni.php?",
-  "display": "standalone",
-  "background_color": "#472300",
-  "theme_color": "#472300",
-  "icons": [
-    {
-      "src": "/images/maskable_icon_x192.png",
-      "sizes": "192x192",
-      "type": "image/png",
-      "purpose": "any"
-    },
-    {
-      "src": "/images/maskable_icon_x512.png",
-      "sizes": "512x512",
-      "type": "image/png",
-      "purpose": "maskable"
-    }
-  ],
-  "id": "atxarber-interrogazioni-programmate",
-  "lang": "it",
-  "display_override": [
-    "window-controls-overlay"
-  ],
-  "categories": [
-    "education",
-    "utilities"
-  ],
-  "description": "Organizza facilmente e rapidamente delle interrogazioni programmate, dando solo date e disponibilità.",
-  "scope": "https://scuola.xcenter.it/"
-}
-*/
 echo json_encode(array(
   "name" => "Interrogazioni Programmate",
   "short_name" => "Interrogazioni",
   "start_url" => $startUrl,
   "display" => "standalone",
+  "orientation" => "any",
   "background_color" => "#472300",
   "theme_color" => "#472300",
   "icons" => array(
@@ -74,5 +40,8 @@ echo json_encode(array(
     "utilities"
   ),
   "description" => "Organizza facilmente e rapidamente delle interrogazioni programmate, dando solo date e disponibilità.",
-  "scope" => "https://scuola.xcenter.it/"
+  "scope" => "https://scuola.xcenter.it/",
+  "launch_handler" => array(
+    "client_mode" => "auto"
+  )
 ));
