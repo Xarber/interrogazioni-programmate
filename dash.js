dc34c3e0f5199fbefe6c26ca224b3c8165e38a84
Xarber <nintendoswitch01234@gmail.com>
2024-10-29 07:14:20 +0100

    CSS Fix again

--- dash.js
+++ dash.js
@@ -917,6 +917,11 @@ class AdminDashboard {
                 background-color: rgba(100, 100, 100, 0.5) !important;
             }
             .admin-switch-to-date:not(.admin-swapping-user-answer > .admin-switch-to-date) {display: none !important;}
+            .admin-switch-to-date {
+                background-color: rgba(0, 0, 0, 0.2) !important;
+                border: 2px rgba(255, 255, 255, 0.7) solid !important;
+                border-style: dashed !important;
+            }
             .admin-day-item:has(.admin-current-swapping-element), .admin-swapping-user-answer > .admin-day-item:hover, .admin-switch-to-date {
                 background-color: rgba(60, 60, 60, 0.3);
                 border: 2px rgba(255, 255, 255, 0.7) solid;
