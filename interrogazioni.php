98507717a6381284c98cf9f9b44840d2b8e963dc
Xarber <62101357+Xarber@users.noreply.github.com>
2024-11-04 00:36:35 +0100

    Update interrogazioni.php

--- interrogazioni.php
+++ interrogazioni.php
@@ -599,7 +599,7 @@ foreach ($subjectJSONs as $subjectNameTMP) {
             const utenti = options.users ?? window.users;
             const datiMateria = options.data ?? <?php echo ($userData["admin"] ?? false) ? json_encode($subjectData) : ""; ?>
             if (!datiMateria) return false;
-            const materia = options.subject ?? <?php echo $subjectName; ?>;
+            const materia = options.subject ?? `<?php echo $subjectName; ?>`;
             const messageArr = [];
             var listaPrenotazioni = {};
 
