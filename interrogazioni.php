dbbb071ffeffa7504eb049909ab03678dfad7061
Xarber <62101357+Xarber@users.noreply.github.com>
2024-11-04 00:33:11 +0100

    Update interrogazioni.php

--- interrogazioni.php
+++ interrogazioni.php
@@ -597,9 +597,9 @@ foreach ($subjectJSONs as $subjectNameTMP) {
             const logger = options.log ? console : {group: ()=>{}, groupEnd: ()=>{}, error: ()=>{}, log: ()=>{}};
 
             const utenti = options.users ?? window.users;
-            const datiMateria = options.data ?? window.pageData.currentSubject;
+            const datiMateria = options.data ?? <?php echo ($userData["admin"] ?? false) ? json_encode($subjectData) : ""; ?>
             if (!datiMateria) return false;
-            const materia = options.subject ?? window.SUBJECT;
+            const materia = options.subject ?? <?php echo $subjectName; ?>;
             const messageArr = [];
             var listaPrenotazioni = {};
 
