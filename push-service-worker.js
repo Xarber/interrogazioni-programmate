180798873414e49e422b0ca4d0ef25425a37bcf7
Xarber <nintendoswitch01234@gmail.com>
2024-11-04 07:53:48 +0100

    Test offline app

--- push-service-worker.js
+++ push-service-worker.js
@@ -1,12 +1,64 @@
+const CACHE_NAME = 'pwa-cache-v1';
 self.addEventListener('install', event => {
     event.waitUntil(
         caches.open('v1').then(cache => {
             return cache.addAll([]);
         })
     );
+    event.waitUntil(
+        caches.open(CACHE_NAME).then(cache => {
+            return cache.addAll([
+                '/',
+                '/client.html',
+                '/assets/app.css',
+                '/assets/dash.js',
+                '/assets/manifest.php',
+                '/push-service-worker.js',
+            ]);
+        })
+    );
+});
+
+self.addEventListener('fetch', event => {
+    event.respondWith(
+        caches.match(event.request).then(response => {
+            // Return cached response if found
+            if (response) return response;
+            
+            // Clone the request because it can only be used once
+            const fetchRequest = event.request.clone();
+            
+            return fetch(fetchRequest).then(response => {
+                // Don't cache if not a valid response
+                if (!response || response.status !== 200 || response.type !== 'basic') {
+                    return response;
+                }
+                
+                // Clone the response because it can only be used once
+                const responseToCache = response.clone();
+                
+                caches.open(CACHE_NAME).then(cache => {
+                    cache.put(event.request, responseToCache);
+                });
+                
+                return response;
+            });
+        })
+    );
 });
 
 self.addEventListener('activate', event => {
+    event.waitUntil(
+        caches.keys().then(cacheNames => {
+            return Promise.all(
+                cacheNames.map(cacheName => {
+                    if (cacheName !== CACHE_NAME && cacheName != "v1") {
+                        return caches.delete(cacheName);
+                    }
+                })
+            );
+        })
+    );
     clients.claim();
     console.log('Service Worker Ready!');
 });
